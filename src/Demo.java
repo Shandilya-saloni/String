d250924729f12c82ea121dc46a89389c4a0c71b8
Shandilya-saloni <114082157+Shandilya-saloni@users.noreply.github.com>
2022-12-28 15:51:01 +0530

    Initial commit

---
+++ src/Demo.java
@@ -0,0 +1,17 @@
+public class Demo {
+    /*public static void main (String[] args){
+        int a = 5;
+        int b = 6;
+        String s = "Hello Dear";
+        String res = a + b + s ;
+        System.out.println(res);
+*/
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        // System.out.println("The sum of " + a + " & " + b + " = " + a + b); // here noth the values are stored it's not showing the sum of both of them
+       // System.out.println("The sum of " + a + " & " + b + " = " + (a + b)); // here we have put bracket so it will give us the sum of both of the integers
+        System.out.println("The sum of " + a + " & " + b + " = " + a*b  ); // here first preference will be given to multiplication according to bodmass same is with division also if it is written + a / b
+    }
+}
+
